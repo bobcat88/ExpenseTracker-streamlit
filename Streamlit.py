12775d9b0b02e6c293eca5d78c68fa894ccb5350
johan.proust <johanproust@gmail.com>
2024-01-22 10:52:54 +0100

    Added Logo, Started work on Streamlit

---
+++ Streamlit.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# Title
+st.title("Budg€t")
+
+# Header
+st.header("Your personal expense tracker")
+
+# sub header
+st.subheader("")
+
+# information
+st.info("first release")
+
+# warning
+st.warning("this version might have bugs and things not working as intended")
+
+#error prompt
+#st.error (".")
+# success prompt
+#st.success(".")
+
+# write
+st.write("Coded by Bobcat88")
